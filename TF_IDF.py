c57033cd1bd5008e1e13d39d1f8ad2627900dad4
JIQA-24 <70869269+JIQA-24@users.noreply.github.com>
2023-04-27 19:20:31 -0600

    new tf_idf

--- TF_IDF.py
+++ TF_IDF.py
@@ -1,20 +1,41 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import math
+import pandas as pd
+import numpy as np
 
-corpus = [
-    'This is the first document.',
-    'This document is the second document.',
-    'And this is the third one.',
-    'Is this the first document?',
-]
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(corpus)
+# Define a set of documents
+docs = ['This is the first document', 'This document is the second document', 'And this is the third one', 'Is this the first document?']
 
-# Similitud de coseno
-cos_sim = cosine_similarity(X)
+# Create a pandas DataFrame with the word counts for each document
+word_counts = pd.DataFrame(index=list(set(word.lower() for doc in docs for word in doc.split())), columns=range(len(docs)), data=0)
+for i, doc in enumerate(docs):
+    words = doc.split()
+    for word in words:
+        word_counts.loc[word.lower(), i] += 1
 
-#print(vectorizer.vocabulary_)
-#print(X.toarray())
+# Calculate the term frequency (TF) for each word in each document
+tf = np.zeros_like(word_counts.values, dtype=float)
+for i in range(len(docs)):
+    tf[:, i] = word_counts.iloc[:, i].values / np.sum(word_counts.iloc[:, i].values)
 
-# Print la matrix de similitud
-print(cos_sim)
+# Calculate the inverse document frequency (IDF) for each word
+idf = np.zeros_like(word_counts.values, dtype=float)
+for i in range(len(word_counts)):
+    df = np.count_nonzero(word_counts.iloc[i].values)
+    idf[i] = math.log(len(docs) / df)
+
+# Calculate the TF-IDF scores for each word in each document
+tf_idf = tf * idf
+
+# Calculate the cosine similarity between each pair of documents
+similarity_matrix = np.zeros((len(docs), len(docs)))
+for i in range(len(docs)):
+    for j in range(len(docs)):
+        if i == j:
+            similarity_matrix[i, j] = 1.0
+        else:
+            similarity_matrix[i, j] = np.dot(tf_idf[:, i], tf_idf[:, j]) / (np.linalg.norm(tf_idf[:, i]) * np.linalg.norm(tf_idf[:, j]))
+
+# Create a DataFrame to display the results
+output_columns = [f"Document {i+1}" for i in range(len(docs))]
+output_df = pd.DataFrame(data=similarity_matrix, columns=output_columns, index=output_columns)
+print(output_df)
